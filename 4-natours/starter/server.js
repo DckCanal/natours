4cf6dad7b9e16d54865da658a5cbc662aae855a8
Marco De Canal <marco.decanal@gmail.com>
2021-03-28 22:26:16 +0200

    mongoose setup and database connection

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,8 +1,24 @@
 // Loading config file... these variables will be accessible from every .js file!
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
 dotenv.config({ path: './config.env' });
 // console.log(process.env);
 // console.log(process.env['LOGNAME']);
+
+const DB = process.env.DATABASE_URL.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    //.connect(process.env.DATABASE_LOCAL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then((connection) => console.log('DB connection successful!'));
 
 // Import the app file only after reading the config file!
 const app = require('./app');
